81a3a5fc3eb27e6de3bf43300a55f7480ea1cd71
Worralop Srichainont <worralop.sric@gmail.com>
2025-09-01 19:50:56 +0700

    feat: add gc problem

---
+++ RS01-Bioinformatics-Stronghold/GC/GC.py
@@ -0,0 +1,27 @@
+# --------------------------------------------------
+# File Name : BA3G.py
+# Problem   : Computing GC Content
+# Author    : Worralop Srichainont
+# Date      : 2025-08-20
+# --------------------------------------------------
+
+import os
+
+# Directory Configurations
+PROBLEM_DIR = os.path.dirname(os.path.abspath(__file__))
+TESTCASE = os.path.join(PROBLEM_DIR, "src", "testcase.txt")
+SRC_FILE = os.path.join(PROBLEM_DIR, "src", "rosalind_gc.txt")
+ANS_FILE = os.path.join(PROBLEM_DIR, "src", "answer.txt")
+
+SOURCES = (TESTCASE, SRC_FILE)
+
+# Prompt user to select source file
+print("========================================")
+print("Current Problem:", os.path.basename(__file__))
+print("========== SELECT SOURCE FILE ==========")
+print("0 for test file")
+print("1 for actual source file")
+selection = int(input("Enter your choice (0 or 1): ").strip())
+print("========================================")
+
+# Initialize counter for
